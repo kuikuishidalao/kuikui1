ed86382bc282ec37d8d5fcfb1cefd5b6592accb4
kuikuishidalao <116491253+kuikuishidalao@users.noreply.github.com>
2022-10-23 19:11:57 +0800

    Add files via upload

---
+++ swiper1.js
@@ -0,0 +1,12 @@
+window.addEventListener("load",function(){
+    var swiper = new Swiper(".mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "progressbar",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+})
